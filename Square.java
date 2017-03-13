e594b9529d7338a16451302a89bc28868bdf741d
zephyrys <cress.milo19@gmail.com>
2017-03-13 11:22:31 -0400

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square {
+    Piece piece;
+    byte xPos, yPos;
+
+    public Square(Piece piece, byte xPos, byte yPos) {
+        this.piece = piece;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
